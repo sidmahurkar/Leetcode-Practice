2cb7c948bd7f6054cffa4727d9bc8d73a33fe0e3
Siddhant Mahurkar <45026019+sidmahurkar@users.noreply.github.com>
2021-05-18 19:24:33 +0530

    Add files via upload

---
+++ Python/easy/270.py
@@ -0,0 +1,23 @@
+class Solution:
+    def removeDuplicates(self, s: str) -> str:
+        
+        result = []
+        
+        for c in s:
+            
+            if result and result[-1] == c:
+                result.pop()
+            else:
+                result.append(c)
+                
+        return "".join(result)
+        
+            
+                    
+        
+            
+            
+                
+            
+                
+        
